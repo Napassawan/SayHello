da869754a3f96bf55e6ed7594705ff828dfa3b2f
Napassawan <lion24092544@gmail.com>
2020-08-10 22:22:45 +0700

    My name

--- SayHello/Source.cpp
+++ SayHello/Source.cpp
@@ -3,7 +3,7 @@ char name[100];
 void main()
 {
 	printf("Oon : What is your name?\n");
-	printf("You : ");
+	printf("You : My name is");
 	scanf("%s", &name);
 	printf("Oon : Hello %s, Nice to meet you.", name);
 }
