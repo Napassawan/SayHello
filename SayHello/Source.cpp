dcdcdca4129faf6d127bd80d5cc8bc7e46a8b29e
Napassawan <lion24092544@gmail.com>
2020-08-10 22:32:58 +0700

    add spacebar

--- SayHello/Source.cpp
+++ SayHello/Source.cpp
@@ -1,9 +1,9 @@
 #include<stdio.h>
 char name[100];
-void main()
+int main()
 {
 	printf("Oon : What is your name?\n");
-	printf("You : My name is");
+	printf("You : My name is ");
 	scanf("%s", &name);
 	printf("Oon : Hello %s, Nice to meet you.", name);
 }
