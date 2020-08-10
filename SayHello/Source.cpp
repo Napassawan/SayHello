d621828407af563bac039df8ec608d97d7d34838
Napassawan <lion24092544@gmail.com>
2020-08-10 22:21:19 +0700

    Add project files.

---
+++ SayHello/Source.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+char name[100];
+void main()
+{
+	printf("Oon : What is your name?\n");
+	printf("You : ");
+	scanf("%s", &name);
+	printf("Oon : Hello %s, Nice to meet you.", name);
+}
